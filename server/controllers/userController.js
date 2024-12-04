2f6d2fa83839f1360e116197a0bc6074df560384
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-12-04 17:07:06 +0530

    Initial commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,88 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
+
+export const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fiels are required!",
+            });
+        };
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exist!",
+            });
+        }
+
+        // Ensure password is a valid string
+        if (typeof password !== 'string') {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid password format!",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        return res.status(201).json({
+            success: true,
+            message: "Account created successfully!",
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to register!",
+        });
+    };
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fiels are required!",
+            });
+        };
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect email or password!",
+            });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect email or password!",
+            });
+        }
+
+        generateToken(res, user, `Welcome back ${user.name}`);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to login!",
+        });
+    };
+};
--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors"
 import connectDB from "./database/db.js";
+import userRoute from "./routes/userRoute.js";
 
-
-dotenv.config({});
-// call database connection here
-connectDB;
 const app = express();
-
+dotenv.config({});
+connectDB(); // call database connection here
 const PORT = process.env.PORT;
+
+// default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:8080",
+    credentials: true
+}));
+
+// apis
+app.use("/api/v1/user", userRoute);
+
+
 app.listen(PORT, () => {
     console.log(`Server is listening at port ${PORT}`);
 });
