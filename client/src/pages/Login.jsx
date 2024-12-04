96e3134c48844f52c9b2843083c247ffe91b316e
Nitish Singh <nitishkumarsingh909@gmail.com>
2024-12-04 13:54:57 +0530

    Initial commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -13,8 +13,25 @@ import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Login = () => {
+  const [signupInput, setSignupInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [loginInput, setLoginInput] = useState({ email: "", password: "" });
+
+  const changeInputHandler = (e, type) => {
+    const [name, value] = e.target;
+
+    if (type === "signup") {
+      setSignupInput({ ...signupInput, [name]: value });
+    } else {
+      setLoginInput({ ...loginInput, [name]: value });
+    }
+  };
+
   return (
-    <div>
+    <div className="flex items-center justify-center w-full">
       <Tabs defaultValue="signup" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="signup">Signup</TabsTrigger>
@@ -29,30 +46,47 @@ const Login = () => {
                 Create a new account and click signup when you're done.
               </CardDescription>
             </CardHeader>
+
             <CardContent className="space-y-2">
               <div className="space-y-1">
                 <Label htmlFor="name">Name</Label>
                 <Input
                   id="name"
                   type="text"
+                  name="name"
+                  value={signupInput.name}
+                  onChange={changeInputHandler}
                   placeholder="Eg. Nitish"
                   required
                 />
               </div>
+
               <div className="space-y-1">
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
                   type="email"
+                  name="email"
+                  value={signupInput.email}
+                  onChange={changeInputHandler}
                   placeholder="abc@gmail.com"
                   required
                 />
               </div>
+
               <div className="space-y-1">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" required />
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={signupInput.password}
+                  onChange={changeInputHandler}
+                  required
+                />
               </div>
             </CardContent>
+
             <CardFooter>
               <Button>Signup</Button>
             </CardFooter>
@@ -67,21 +101,34 @@ const Login = () => {
                 Login your account here. After signup, you'll be logged in.
               </CardDescription>
             </CardHeader>
+
             <CardContent className="space-y-2">
               <div className="space-y-1">
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
                   type="email"
+                  name="email"
+                  value={loginInput.email}
+                  onChange={changeInputHandler}
                   placeholder="abc@gmail.com"
                   required
                 />
               </div>
+
               <div className="space-y-1">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" required />
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={loginInput.password}
+                  onChange={changeInputHandler}
+                  required
+                />
               </div>
             </CardContent>
+
             <CardFooter>
               <Button>Login</Button>
             </CardFooter>
